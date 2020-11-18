b7c6ced2e9f948d3714bd14b819c15a63bb7db2f
mazatf2 <2814748+mazatf2@users.noreply.github.com>
2020-11-18 20:04:45 +0200

    feat: show per log stats

--- src/components/combinelogs/RoundInfoRoundOverview.tsx
+++ src/components/combinelogs/RoundInfoRoundOverview.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {Row} from 'react-table'
 import {logstfJson} from '../../logstf_api'
+import {CombineLogsPlayersTable} from './CombineLogsPlayersTable'
 
 export const RoundInfoRoundOverview = (row: Row, index: number) => {
 	const log = row.original as logstfJson
-	return <div>temp</div>
+	
+	return <>
+		<span>{log.info.title} - {log.info.map}</span>
+		<CombineLogsPlayersTable logsArr={[log]} steam32=""/>
+	</>
 }
